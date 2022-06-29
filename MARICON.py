d5a3a22e40630e902d01ed040f6aec18fcd3b463
yooanan <97883579+yooanan@users.noreply.github.com>
2022-06-29 23:25:04 +0200

    Update MARICON.py

--- MARICON.py
+++ MARICON.py
@@ -1,6 +1,6 @@
 import random
 import numpy as np
-import pandas pd
+import pandas as pd
 
 ## empty list to append 
 x=[]
@@ -10,5 +10,14 @@ for i in range(0,10):
   
   
 df = pd.DataFrame(np.random.randint(0,10000,size=(10,500)))
+
+da=[]
+for i in range(0,10):
+  for j in range(0,500):
+    da.append(df.iloc[int(i),int(j)]>x[i])
+
+da2=np.reshape(da,(10,500))
+
+pdata=pd.DataFrame(data=da2,columns=['col1','col2','col3','col4','col5','col6','col7','col8','col9','col10'])
 
 
