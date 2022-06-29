49135a63abe399c82c6987b7e8474866073f3c28
yooanan <97883579+yooanan@users.noreply.github.com>
2022-06-29 22:52:38 +0200

    Update MARICON.py

--- MARICON.py
+++ MARICON.py
@@ -1,9 +1,14 @@
 import random
+import numpy as np
 import pandas pd
 
 ## empty list to append 
 x=[]
 for i in range(0,10):
-  x.append(randint(0,1000))
+  x.append(randint(0,10000))
+
   
   
+df = pd.DataFrame(np.random.randint(0,10000,size=(10,500)))
+
+
